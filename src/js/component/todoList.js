8fcfe5b5c5e2bf947c45df86709d00b2ca80d78d
soydeif <daed_diaz@hotmail.com>
2021-06-22 13:04:00 +0000

    1

--- src/js/component/todoList.js
+++ src/js/component/todoList.js
@@ -1,34 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Alert } from "bootstrap";
 
-// necesito localizar el index de "result" para poder controlar la iteracion de las tareas
-// una vez tenga cada key o index de ellas, necesito poder hacer un evento que me permita eliminar dicha tarea.
-// he decidido crear un boton que general que me permita eliminar la primera tarea añadida del array pero no funciona ya que devuelve NaN - result.lengt-1
-// he tratado con for pero no he tenido exito
+// añadir tareas tanto con click como enter -* search google event onKeyPress *-
+// lograr que la barra elimine la ultima tarea del placeholder.
 
 export function TodoList() {
-	const [task, setTask] = useState([
-		{
-			id: "1",
-			name: " Go out for dinner"
-		},
-		{
-			id: "2",
-			name: " Check the boiler"
-		}
-	]);
-	const [addList, setAddList] = useState([]);
+	const [task, setTask] = useState([]);
+	const [addList, setAddList] = useState("");
+
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/soydeif")
+			.then(response => response.json())
+			.then(data => setTask(data));
+	}, []);
+	useEffect(() => {
+		update();
+		console.log("HERE");
+	}, [task]);
+
+	const update = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/soydeif", {
+			method: "PUT",
+			body: JSON.stringify(task), // data can be `string` or {object}!
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(res => res.json())
+			.catch(error => console.error("Error:", error))
+			.then(response => console.log("Success:", response));
+	};
+
 	const result = task.map((item, index) => (
 		<li key={index}>
-			{item.name}
+			{item.label}
 			<button
 				onClick={e => {
-					setTask(
-						task.filter(
-							singleTask =>
-								singleTask.id !== e.target.dataset.taskId
-						)
-					);
+					setTask(task.filter((singleTask, i) => i !== index));
 				}}
 				className="btn btn-outline-secondary"
 				data-task-id={item.id}
@@ -44,6 +52,7 @@ export function TodoList() {
 			<div className="input-group mb-3">
 				<input
 					type="text"
+					value={addList}
 					onChange={e => setAddList(e.target.value)} // indexa la tarea desde la caja de texto hasta la lista UL
 					className="form-control"
 					placeholder="Things to do"
@@ -53,11 +62,9 @@ export function TodoList() {
 				<div className="input-group-append">
 					<button
 						onClick={e => {
-							//e.keyCode === "&#13" ||
-							setTask([
-								...task,
-								{ id: Date.now().toString(), name: addList }
-							]);
+							setTask([...task, { label: addList, done: false }]); //actualiza el valor de task con un array,(hay corchetes, obviamente) // se añade al array addList el label y el done que vienen del nuevo array
+
+							setAddList("");
 						}}
 						className="btn btn-outline-secondary"
 						type="button"
